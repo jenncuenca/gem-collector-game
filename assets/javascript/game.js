48ddc7fc73998eefb35b92c24223a995f7aab295
Jennifer Cuenca <>
2018-02-16 22:50:52 -0500

    minor html updates // got gems to generate a random number when clicked (still need to get them to add up)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,53 @@
-
-
+$(document).ready(function() {
+ 
+// Variables
 var wins = 0;
 var loses = 0;
 var userGemTotal = 0;
 
-
-
-$(document).ready(function() {
-
-//Random Number Gets Generated
+//Random Number gets generated
 var numToBeMatched = 19 + Math.floor(Math.random()*120);
 $("#random-number").text(numToBeMatched);
 
 //WIN CONDITION: If user Gem clicks match the Randomly Generated Number 
-if (userGemTotal === NumToBeMatched){
+if (userGemTotal === numToBeMatched){
     wins += 1; // wins increase by 1.
     alert("Congratulations! YOU WIN!!"); // informs user of win
 }
 
 //LOSS CONDITION: If user Gem clicks are more than Randomly Generated Number
-else if (userGemTotal > NumToBeMatched) {
+else if (userGemTotal >= numToBeMatched){
     losses += 1; // losses increase by 1.
+    alert("You lose!"); // informs user of loss
 }
 
+// gems generate a random number
 
-// gems generate a random number that is unseen
-$("#random-button").on("click", function(){
-    var random = Math.floor(Math.random() * 12) + 1;
-    $("#random-number").text(random);
-    $("#random-number").append("<br>");
-  })
+//Gem 1
+$("#greenGem").on("click", function(){
+    var gemValue = Math.floor(Math.random() * 12) + 1;
+    $("#playerScore").text(gemValue);
+  });
 
-//gem buttons?
-$(".normal-button").on("click", function() {
-    //value generated
-});
+//Gem 2
+$("#pinkGem").on("click", function(){
+    var gemValue = Math.floor(Math.random() * 12) + 1;
+    $("#playerScore").text(gemValue);
+  });
+
+//Gem 3
+
+$("#blueGem").on("click", function(){
+    var gemValue = Math.floor(Math.random() * 12) + 1;
+    $("#playerScore").text(gemValue);
+  });
+
+//Gem 4
+$("#orangeGem").on("click", function(){
+    var gemValue = Math.floor(Math.random() * 12) + 1;
+    $("#playerScore").text(gemValue);
+  });
 
 //score tracking
 
-})
+});
